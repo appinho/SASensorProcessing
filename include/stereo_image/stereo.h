11ee62e248a6cc547e3793786d20a3c0b3c54e37
appinho <simonappel62@gmail.com>
2020-03-01 11:46:12 +0100

    Added subscriber policy

--- include/stereo_image/stereo.h
+++ include/stereo_image/stereo.h
@@ -5,8 +5,18 @@
 #include <dynamic_reconfigure/server.h>
 #include <stereo_image/StereoParamsConfig.h>
 
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+
 namespace stereo_image
 {
+
+using namespace sensor_msgs;
+using namespace message_filters::sync_policies;
+
 class Stereo
 {
  public:
@@ -17,6 +27,19 @@ class Stereo
   void configCallback(stereo_image::StereoParamsConfig &config, uint32_t level);
 
   ros::NodeHandle nh_;
+
+  message_filters::Subscriber<Image> sub_left_image_;
+  message_filters::Subscriber<CameraInfo> sub_left_camera_info_;
+  message_filters::Subscriber<Image> sub_right_image_;
+  message_filters::Subscriber<CameraInfo> sub_right_camera_info_;
+  typedef ExactTime<Image, CameraInfo, Image, CameraInfo> ExactPolicy;
+  typedef message_filters::Synchronizer<ExactPolicy> ExactSync;
+  ExactSync exact_sync_;
+
+  void callback(const ImageConstPtr& l_image_msg,
+                const CameraInfoConstPtr& l_info_msg,
+                const ImageConstPtr& r_image_msg,
+                const CameraInfoConstPtr& r_info_msg);
 
   ros::Publisher pub_;
 
--- src/stereo.cpp
+++ src/stereo.cpp
@@ -3,7 +3,10 @@
 namespace stereo_image
 {
 Stereo::Stereo(ros::NodeHandle nh) : 
-  nh_(nh)
+  nh_(nh),
+  exact_sync_(ExactPolicy(10),
+    sub_left_image_, sub_left_camera_info_,
+    sub_right_image_, sub_right_camera_info_)
 {
   // Set up a dynamic reconfigure server.
   // Do this before parameter server, else some of the parameter server values can be overwritten.
@@ -19,6 +22,21 @@ Stereo::Stereo(ros::NodeHandle nh) :
   pnh.param("numDisparities", num_disparities_, num_disparities_);
   pnh.param("blockSize", block_size_, block_size_);
 
+  sub_left_image_.subscribe(nh_, "kitti/camera_gray_left/image_raw", 1);
+  sub_left_camera_info_.subscribe(nh_, "/kitti/camera_gray_left/camera_info", 1);
+  sub_right_image_.subscribe(nh_, "/kitti/camera_gray_right/image_raw", 1);
+  sub_right_camera_info_.subscribe(nh_, "/kitti/camera_gray_right/camera_info", 1);
+  exact_sync_.registerCallback(boost::bind(&Stereo::callback,
+                                              this, _1, _2, _3, _4));
+}
+
+void Stereo::callback(const ImageConstPtr& l_image_msg,
+                      const CameraInfoConstPtr& l_info_msg,
+                      const ImageConstPtr& r_image_msg,
+                      const CameraInfoConstPtr& r_info_msg)
+{
+
+  ROS_INFO("Callback");
 }
 
 void Stereo::configCallback(stereo_image::StereoParamsConfig &config, uint32_t level __attribute__((unused)))
